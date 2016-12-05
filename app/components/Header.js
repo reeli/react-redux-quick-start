433c5f258310ee906ddffd0505dbe202e4e8c63a
HelloRuiLi <1229668446@qq.com>
2016-12-05 15:22:01 +0800

    resolve merge conflict

--- app/components/Header.js
+++ app/components/Header.js
@@ -3,13 +3,8 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-<<<<<<< HEAD
-      <div className={Header_.bg}>Header</div>
-    );
-=======
       <div>Header</div>
     )
->>>>>>> add test
   }
 }
 
